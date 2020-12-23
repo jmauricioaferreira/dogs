81bcd05dcf1b627b0d949d1d7c20a6a11e8362bb
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-23 13:15:14 -0300

    create protected route

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-
-import "./App.css";
 import { UserStorage } from "./UserContext";
+import { User } from "./Components/User/User";
+import "./App.css";
+import { ProtectedRoute } from "./Components/Helper/ProtectedRoute";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ src/Components/Helper/ProtectedRoute.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navigate, Route } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+
+export const ProtectedRoute = (props) => {
+  const { login } = React.useContext(UserContext);
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
+};
