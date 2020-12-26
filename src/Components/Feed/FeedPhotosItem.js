667de010718b65323cbc4148c92881eb8952efa8
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-26 15:02:39 -0300

    creating image skeleptom

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
+import Image from "../Helper/Image";
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
     setModalPhoto(photo);
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
