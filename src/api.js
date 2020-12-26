5efbbb8c8b003ad435cd93a26b4778acd797a318
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-26 14:28:39 -0300

    creating and styling PhotoDelete

--- src/api.js
+++ src/api.js
@@ -95,3 +95,15 @@ export function COMMENT_POST(id, body) {
     },
   };
 }
+
+export function PHOTO_DELETE(id, body) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+    },
+  };
+}
