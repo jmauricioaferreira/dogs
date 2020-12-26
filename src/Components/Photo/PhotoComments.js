2dfb1975bdbcd2d065bdc52280cc5e08fa616683
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-26 10:58:31 -0300

    creating PhotoComment and PhotoComentForm

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+  return (
+    <>
+      <ul className={styles.comment}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}:</b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
