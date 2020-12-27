480ad052cb8588650eb01a488cb72a9a047d25d6
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-27 12:18:17 -0300

    creating UserProfile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { User } from "./Components/User/User";
 import "./App.css";
 import { ProtectedRoute } from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
