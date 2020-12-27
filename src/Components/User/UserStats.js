7bd42cdb541be64a5b1c8b166c1d7706e80495c8
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-27 16:30:23 -0300

    crating and styling UserStats

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,30 @@
 import React from "react";
+import useFetch from "../../Hooks/useFetch";
 import Head from "../Helper/Head";
+import { STATS_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
--- src/api.js
+++ src/api.js
@@ -132,3 +132,15 @@ export function PASSWORD_RESET(body) {
     },
   };
 }
+
+export function STATS_GET() {
+  return {
+    url: API_URL + "/api/stats",
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+    },
+  };
+}
